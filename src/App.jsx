625d1e60a412dd59245bca57a0049a20b512edd5
Ayudh <ayudhnandi@gmail.com>
2025-01-09 12:17:58 +0530

    updated example props

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-// import "./App.css";
 import TextCanvas from "./TextCanvas";
 function App() {
   return (
     <TextCanvas
-      texts={["Hi...", "Welcome", "To", "Typing Animation"]}
-      textcolor="white"
+      texts={["Hello..!!", "Welcome", "To", "Typing Animation"]}
+      textcolor="#B6FFFA"
       fontSize={20}
       displayCaret={1}
       caretChar={"|"}
       speeds={{
-        type: 500,
-        pause: 2000,
-        clear: 500,
-        delay: 800,
+        type: 150, //sets typing speed in milliseconds
+        pause: 1500, //sets the duration of pause after a string is typed
+        clear: 150, //sets the speed at which the text is cleared
+        delay: 500, //sets the pause duration between one text cleared and the next text being typed
       }}
     />
   );
